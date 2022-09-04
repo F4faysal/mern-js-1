60871da99c480b8073815bbc636bfbdabcf355ab
F4faysal <101200085+F4faysal@users.noreply.github.com>
2022-09-04 21:48:44 +0600

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Import stylesheets
 import './style.css';
 
+console.log('faysal');
+console.log('add',10+10)
+
+
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>Mern-JS-1</h1>`;
